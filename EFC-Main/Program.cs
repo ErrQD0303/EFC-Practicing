52b286e1ff1a0ea79ec87a363c53e643a7a6bb2a
ErrQD0303 <quocdat3396@gmail.com>
2024-12-12 03:13:16 +0700

    Complete Entity Framework Model Creation

--- EFC-Main/Program.cs
+++ EFC-Main/Program.cs
@@ -2,7 +2,7 @@
 
 await ClassExamples.DeleteDatabase();
 await ClassExamples.CreateDatabase();
-await ClassExamples.InsertRecordsToProductTable();
+await ClassExamples.InsertRecords();
 await ClassExamples.ReadRecordsFromProductTable();
 await ClassExamples.UpdateRecordInProductTable();
 await ClassExamples.DeleteRecordFromProductTable();
